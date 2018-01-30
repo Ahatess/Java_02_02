3a1d9de3a419ff3bfaa49a19e6df93b0dca75045
Andrius <andriuscere@gmail.com>
2018-01-30 14:55:00 +0200

    Antra užduotis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+    System.out.println("Irasykite zodi");
+    Scanner sc = new Scanner(System.in);
+    String zodis = sc.nextLine();
+    System.out.println(zodis.length());
     }
 }
